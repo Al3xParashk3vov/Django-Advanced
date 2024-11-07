dd0860c61df0dd079d5c775d8fa6c45b06699a40
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-11-07 15:43:45 +0200

    Ending 2nd lesson

--- forumApp/settings.py
+++ forumApp/settings.py
@@ -81,13 +81,9 @@ WSGI_APPLICATION = 'forumApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "forumApp",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -136,3 +132,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
